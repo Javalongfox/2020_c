50dca128019fd6d1eb2f48c595a104702fd494ba
Javalongfox <76526822+Javalongfox@users.noreply.github.com>
2020-12-29 19:53:46 +0300

    Add files via upload

---
+++ Kp.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int arr[100][100];
+    int n, m, index_min_i = 0, index_min_j = 0 , i=1,j=1;
+    printf_s("Введите размерность первого массива: ");
+ //   do 
+ //   {
+    n = 5;
+    m = 5;
+//    } while ((n < 0) && (m < 0));
+        printf_s("Введите первую матрицу: \n");
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                arr[i][j] = rand()% 200+(-100);
+                printf_s("|%d ", arr[i][j]);
+                
+            }
+            printf_s("\n");
+        }
+    int min = arr[0][0];
+   
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        { 
+            if ((arr[i][j] < 0) && (arr[i][j] < min))
+                {
+                    index_min_i = i;
+                    index_min_j = j;
+                    min = arr[i][j];
+                }
+        }
+    }
+    printf_s("Выводим значение минимального элемента\n");
+    //arr[index_min_i][index_min_j] = min;
+    
+    printf_s("%d \n", min );
+    printf_s("Выведем позицию минимального отрицательного элемента \n");
+    printf_s("Строка: ");
+    printf_s("%d \n", index_min_i+1);
+    printf_s("Столбец:");
+    printf_s("%d", index_min_j+1);
+    
+    return(0);
+}
---
+++ laba4.2.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+int main()
+{
+    FILE* file, * file2;
+    char string[1024], string2[1024], * word, * word1;
+
+    if ((file = fopen("In.txt", "r")) == NULL)
+    {
+        puts("ERROR1");
+    }
+    if ((file2 = fopen("In2.txt", "w")) == NULL) 
+    { 
+        puts("ERROR2");
+    }
+
+/*    while (fgets(string, 35 , file) != NULL) 
+    {
+        for (int i = 0; i < sizeof(string) - 1; i++)
+        {
+            string2[i] = string[i];
+        }
+        puts(string); puts(" - "); puts(string2);
+        word = strtok(string, " ,");
+        while (word != NULL)
+        {
+            word1 = strtok(string2, " ,");
+            while (word1 != NULL)
+            {
+                printf("3 - ");
+                printf("-%s-%s-\n",word, word1);
+                for (int i = 0; i < sizeof(word)-1; i++)
+                {
+                    if (word[i] != word1[i]) break;
+                }                          
+                puts("GET\n");
+                word1 = strtok(0, " ,");
+            }
+//            fprintf(file2, "%s " ,word);
+            word = strtok(0, " ,");
+        }
+    }*/
+    fclose(file);
+    fclose(file2);
+    return 0;
+} 
